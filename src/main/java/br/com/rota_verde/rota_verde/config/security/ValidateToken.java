03f9b2c56dbda2fd6874fbbcbbd6e49b7bc54090
Renan Fresatto <renan.martins@germinare.org.br>
2026-05-25 23:31:59 -0300

    fixing ValidateToken

--- src/main/java/br/com/rota_verde/rota_verde/config/security/ValidateToken.java
+++ src/main/java/br/com/rota_verde/rota_verde/config/security/ValidateToken.java
@@ -1,13 +1,14 @@
 package br.com.rota_verde.rota_verde.config.security;
 
-import br.com.rota_verde.rota_verde.user.model.UserModel;
 import br.com.rota_verde.rota_verde.user.repository.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -24,20 +25,20 @@ public class ValidateToken extends OncePerRequestFilter {
     private UserRepository userRepository;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         String authorizationHeaders = request.getHeader("Authorization");
-        String token = "";
 
-        if (authorizationHeaders == null) {
-            token = null;
-        } else {
-            token = authorizationHeaders.replace("Bearer", "").trim();
+        if (authorizationHeaders != null) {
+            String token = authorizationHeaders.replace("Bearer", "").trim();
             String login = tokenService.validateToken(token);
 
-            UserDetails user = userRepository.findByEmail(login);
+            if (login != null && !login.isEmpty()) {
+                UserDetails user = userRepository.findByEmail(login);
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
 
-            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
         }
 
         filterChain.doFilter(request, response);
